f5a1612436a3366317395751122f2c1cef5e398c
plotiwitolp <plotiwitolp@gmail.com>
2023-06-26 14:59:28 +0400

    update

--- page-about.php
+++ page-about.php
@@ -5,6 +5,18 @@
         <h1 class="page-title">О компании</h1>
     </div>
 
+    <section>
+        <div class="container">
+            <div class="block">
+                <div class="about-top">
+                    <div class="about-top-slider">
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 </div>
 
 <?php require_once './footer.php'; ?>
