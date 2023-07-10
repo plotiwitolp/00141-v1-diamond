085102e932103d7d7441ba8719acd940e0ab3c4f
plotiwitolp <plotiwitolp@gmail.com>
2023-07-11 00:37:28 +0400

    update feedback form

--- templates/feedback-form-block.php
+++ templates/feedback-form-block.php
@@ -1,14 +1,1 @@
-<form action="">
-
-    <div class="feedback-form___top">
-        <input type="text" placeholder="Ваше имя">
-        <input type="text" placeholder="Ваш телефон">
-        <input type="text" placeholder="Ваша компания">
-    </div>
-    <div class="feedback-form___bottom">
-        <textarea placeholder="Сообщение"></textarea>
-        <input type="file" name="fileInput" id="fileInput">
-        <input class="diamond-btn" type="submit" value="Отправить">
-    </div>
-
-</form>
+<?php echo do_shortcode('[contact-form-7 id="9" title="Контактная форма"]');
